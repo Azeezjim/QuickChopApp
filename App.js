c2a5d2cfa5388eab4e11b312bd9fd9d570de2844
Azeezjim <azeexjim@gmail.com>
2023-09-02 11:02:53 -1100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar, Text } from 'react-native';
+import { AuthStack } from './src/navigation/authStack';
+import RestaurantHomeScreen from './src/screens/RestaurantHomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import RootNavigator from './src/navigation/rootNavigation';
+import MyOrdersScreen from './src/screens/MyOrdersScreen';
+import { AppStack } from './src/navigation/appStack';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+          // backgroundColor={colors.statusbar}
+        />
+        {/* <RootNavigator /> */}
+        {/* <Text>hello</Text> */}
+        <RootNavigator />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {flex: 1},
 });
